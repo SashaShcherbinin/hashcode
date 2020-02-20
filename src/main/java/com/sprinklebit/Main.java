c95201bd1a21e7cdb5bda33097eda636ce9ba413
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2020-02-20 21:34:45 +0200

    ReaderWriter

--- src/main/java/com/sprinklebit/Main.java
+++ src/main/java/com/sprinklebit/Main.java
@@ -15,11 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("test");
-//        try {
-//            ReaderWriter.write("fffaaa", "result");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
         String fileName = "a_example.txt";
         List<List<String>> s = ReaderWriter.read(fileName);
