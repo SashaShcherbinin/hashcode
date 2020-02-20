4b846c32e0130d519ea146fdb4311d7e58efc04a
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2020-02-20 21:41:23 +0200

    -

--- src/main/java/com/sprinklebit/AlexCalculation.java
+++ src/main/java/com/sprinklebit/AlexCalculation.java
@@ -1,11 +1,14 @@
 package com.sprinklebit;
 
+import com.sprinklebit.output.Info;
 import com.sprinklebit.output.Result;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AlexCalculation {
-    public static Result cacluclate(List<Library> libs, int librariesCount, int maxDays) {
 
+    public static Result cacluclate(List<Library> libs, int librariesCount, int maxDays) {
+        return new Result(0, new ArrayList<Info>());
     }
 }
