d34e5fe3bc4ac621c711fde7d2e1d426ace11b6d
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2020-02-20 19:27:49 +0200

    ReaderWriter

--- src/main/java/com/sprinklebit/Main.java
+++ src/main/java/com/sprinklebit/Main.java
@@ -1,9 +1,15 @@
 package com.sprinklebit;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("test");
-
+        try {
+            ReaderWriter.write("fff", "result");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
