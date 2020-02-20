68856d984f9e57a9e09139f566f4bef425637bdb
s.mykhailov <s.mykhailov@runple.com>
2020-02-20 19:35:46 +0200

    aaa

--- src/main/java/com/sprinklebit/Main.java
+++ src/main/java/com/sprinklebit/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         try {
-            ReaderWriter.write("fff", "result");
+            ReaderWriter.write("fffaaa", "result");
         } catch (IOException e) {
             e.printStackTrace();
         }
