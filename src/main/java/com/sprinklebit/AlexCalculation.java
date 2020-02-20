6a0a3601b130f49e61d928cbefabb28c168df635
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2020-02-20 23:20:21 +0200

    -

--- src/main/java/com/sprinklebit/AlexCalculation.java
+++ src/main/java/com/sprinklebit/AlexCalculation.java
@@ -29,7 +29,7 @@ public class AlexCalculation {
 
     private static Result mapResult(List<Library> libraryList) {
         List<Info> infos = new ArrayList<>();
-        for (int i=0; i<libraryList.size(); i++) {
+        for (int i = 0; i < libraryList.size(); i++) {
             List<Integer> ids = Arrays.asList(libraryList.get(i).booksIds);
             Info info = new Info(libraryList.get(i).id, libraryList.get(i).booksIds.length, ids);
             infos.add(info);
@@ -53,49 +53,21 @@ public class AlexCalculation {
     }
 
     static Set<Integer> getUsedBooks(Library library, int maxDays) {
-        Set<Integer> result = null;
-        int bestScore = 0;
-        int[] leastScore = Main.scoreList.clone();
-
-        int count = 0;
-        int bestScoreId = getIndexOfLargest(leastScore);
-
-        while (count < library.booksIds.length && maxDays > 0) {
-            result.add(library.booksIds[bestScoreId]);
-            if (count % library.booksPerDay == 0) {
-                maxDays--;
+        int maxBooks = maxDays * library.booksPerDay;
+        HashSet<Integer> userBooks = new HashSet<>();
+        for (int i = 0; i < maxBooks; i++) {
+            int bestBookScore = 0;
+            int bestBookId = 0;
+            for (int bookId : library.booksIds) {
+                int score = Main.scoreList[bookId];
+                if (score > bestBookScore) {
+                    bestBookScore = score;
+                    bestBookId = bookId;
+                }
             }
-            count++;
-            leastScore = removeTheElement(leastScore, bestScore);
-            bestScoreId = getIndexOfLargest(leastScore);
+            userBooks.add(bestBookId);
         }
-        return result;
-    }
 
-    public static int getIndexOfLargest(int[] array) {
-        if (array == null || array.length == 0) return -1;
-
-        int largest = 0;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > array[largest]) largest = i;
-        }
-        return largest;
-    }
-
-    public static int[] removeTheElement(int[] arr, int index) {
-        if (arr == null
-                || index < 0
-                || index >= arr.length) {
-
-            return arr;
-        }
-        int[] anotherArray = new int[arr.length - 1];
-        for (int i = 0, k = 0; i < arr.length; i++) {
-            if (i == index) {
-                continue;
-            }
-            anotherArray[k++] = arr[i];
-        }
-        return anotherArray;
+        return userBooks;
     }
 }
