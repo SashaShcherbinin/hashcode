eb653a8f5707f5ddd9784dbd879e216433fa9f68
sasha.shcherbinin <sasha.shcherbinin@gmail.com>
2020-02-20 20:58:55 +0200

    ReaderWriter

--- src/main/java/com/sprinklebit/Main.java
+++ src/main/java/com/sprinklebit/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Main {
 
     public static int[] scoreList;
+    public static List<Integer> scannedBooks = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("test");
